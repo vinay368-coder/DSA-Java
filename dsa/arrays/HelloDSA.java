1f797833dbc5e79a995d5fdad28c6bb9f5e9b8b9
vinay <vinay@Vinay.lan>
2026-03-25 18:17:48 +0530

    hello dsa

--- dsa/arrays/HelloDSA.java
+++ dsa/arrays/HelloDSA.java
@@ -1,7 +1,7 @@
 package dsa.arrays;
 
 public class HelloDSA {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         System.out.println("Starting my DSA journey 🚀");
     }
 }  
